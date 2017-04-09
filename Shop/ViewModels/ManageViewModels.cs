ee673e2a917c36defe0b8f3ec7eb07b842fed757
Łukasz <luqa91@hotmail.com>
2017-04-09 23:52:06 +0200

    Data user modification, Manage panel

---
+++ Shop/ViewModels/ManageViewModels.cs
@@ -0,0 +1,41 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Shop.ViewModels
+{
+    public class ManageViewModels
+    {
+        public class ManageCredentialsViewModel
+        {
+            public ChangePasswordViewModel ChangePasswordViewModel { get; set; }
+            public Shop.Controllers.ManageController.ManageMessageId? Message { get; set; }
+            public DataUser DataUser { get; set; }
+
+        }
+
+
+        public class ChangePasswordViewModel
+        {
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name = "Bieżące hasło")]
+            public string OldPassword { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "{0} musi mieć co najmniej następującą liczbę znaków: {2}.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Nowe hasło")]
+            public string NewPassword { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Potwierdź nowe hasło")]
+            [Compare("NewPassword", ErrorMessage = "Nowe hasło i potwierdzenia hasła nie są zgodne.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+    }
+}
